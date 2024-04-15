bafcf24e65626dffeb7a3ba3b586bb37d32a4399
Moosadeveloper <janmoosa77@gmail.com>
2024-04-15 21:45:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var calculatedNum = 2 + (2 * 6);
+// Answer is 14
+
+var calculatedNum = (2 + 2) * 6;
+// Answer is 24
+
+var calculatedNum = (2 + 2) * (4 + 2);
+// Answer is 24
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+// Answer is 18
+
+
+var calculatedNum = 9 + 3 * 4 + 8 ;
+// Answer is 56
+
+var calculatedNum = 2 + 2 * 4 + 4 ;
+// Answer is 20
+
+var a = 4 /4  * 5 ;
+// Answer is 5
